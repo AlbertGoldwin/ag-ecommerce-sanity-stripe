2510659b03de4c3b030659e5a7dcbdfc8ddca04a
AlbertGoldwin <albert.goldwin001@gmail.com>
2022-11-11 19:18:49 +0700

    slight change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 AG Headphones, All rights reserved.</p>
+      <p>2022 AG Tech, All rights reserved.</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
